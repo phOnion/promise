1e923778c9f33b1694f1fb4ebfab9ac76003bce8
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-01-21 01:07:48 +0200

    Make promise `resolve` reject `private`

--- src/Framework/Promise/Interfaces/PromiseInterface.php
+++ src/Framework/Promise/Interfaces/PromiseInterface.php
@@ -8,9 +8,6 @@ interface PromiseInterface extends ThenableInterface
     public function otherwise(Closure $onRejected): PromiseInterface;
     public function finally(Closure ...$final): PromiseInterface;
 
-    public function resolve($value): void;
-    public function reject(\Throwable $reason): void;
-
     public function isPending(): bool;
     public function isFulfilled(): bool;
     public function isRejected(): bool;
