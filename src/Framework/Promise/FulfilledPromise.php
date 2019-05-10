bfdfb85ae4c5756e76496dc512d7e3dc60947fd7
DaGhostman <daghostman.dd@gmail.com>
2019-05-10 15:28:35 +0300

    Mark argument as mixed

--- src/Framework/Promise/FulfilledPromise.php
+++ src/Framework/Promise/FulfilledPromise.php
@@ -3,6 +3,7 @@ namespace Onion\Framework\Promise;
 
 class FulfilledPromise extends Promise
 {
+    /** @param mixed $value */
     public function __construct($value)
     {
         parent::__construct(function ($resolve, $reject) use ($value) {
